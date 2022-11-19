2a5f6184515de8aae9eb8ca047f35c584e931ed1
nandinhomsf <fernandomsf@id.uff.br>
2022-11-19 18:49:30 -0300

    UDP Server (maybe need to add the ip host parameter)w

--- Server_module_UDP.py
+++ Server_module_UDP.py
@@ -1,4 +1,5 @@
-from Cliente import *
+import socket
+import pyaudio
 
 
 def cli_ser():
@@ -9,18 +10,31 @@ def cli_ser():
     s.bind((host, port))
 
     while True:
+        s.listen()
         print('waiting to receive message')
         data, address = s.recvfrom(1024)
-        print('received: ' + data + '\nfrom: ' + address[0] + '\nlistening on port: ' + str(address[1]))
+        addressAux = address
+        if address is not None:
+            print('received: ' + data + '\nfrom: ' + address[0] + '\nlistening on port: ' + str(address[1]))
+            while True:
 
-        if data == 'convite':
-            resposta = input()
-            s.sendto(resposta, address)
-        elif data == 'encerrar_ligação':
-            break
-        elif data == 'respsota_ao_convite':
-            data = input()
-            s.sendto(data, address)
-
-
-cli_ser()
+                if addressAux != address:
+                    s.sendto('rejeitado', address)
+                elif data == 'convite':
+                    print('responda com aceito ou reijeitado')
+                    resposta = input()
+                    s.sendto(resposta, address)
+                    data = 0
+                elif data == 'encerrar_ligação':
+                    break
+                elif data == 'aceito':
+                    audio = pyaudio.PyAudio()
+                    output_stream = audio.open(format=pyaudio.paInt16, output=True, rate=44100, channels=1,
+                                               frames_per_buffer=4096)
+                    while True:
+                        data, address = s.recvfrom(1024)
+                        output_stream.write(data)
+                        if data == 'encerrar_ligação':
+                            break
+                elif data == 'rejeitado':
+                    break
