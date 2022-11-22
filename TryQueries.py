c88963ecfd26185bf5bdaf4f35284c62b618229f
Dario Mousinho <dariomvpm@id.uff.br>
2022-11-21 23:42:25 -0300

    Some fixes

--- TryQueries.py
+++ TryQueries.py
@@ -4,12 +4,5 @@ cursor = banco.cursor()
 name = "Joao"
 ip = "127.0.0.1"
 port = 6789
-cursor.execute("SELECT nome, ip, porta FROM registros "
-                   f"WHERE nome = '{name}' AND ip = '{ip}'")
-
-print(cursor.fetchall())
-print("Mostrar tudo:")
-name = "Dario"
-cursor.execute("SELECT nome, ip, porta FROM registros "
-                   f"WHERE nome = '{name}'")
+cursor.execute("SELECT * FROM registros")
 print(cursor.fetchall())
